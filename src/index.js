21768aeb188a41a27539c87d07f01711a0088a17
Alexey <ogobel@icloud.com>
2021-02-28 13:42:46 +0300

    feat: add typ arr prblms

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array|| array[0]==undefined ){
+    return 0;
+  }
+  array.sort(function(a, b){
+    if(a < b) { return -1; }
+if(a > b) { return 1; }
+return 0;
+})
+return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+ if(!array|| array[0]==undefined ){
+   return 0;
+ }
+  array.sort(function(a, b){
+    if(a < b) { return -1; }
+if(a > b) { return 1; }
+return 0;
+  });
+    return array[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array|| array[0]==undefined ){
+    return 0;
+  }
+  let sum = 0;
+  array.forEach(element => {
+    sum+=element;
+  });
+  return sum/array.length;
 }
